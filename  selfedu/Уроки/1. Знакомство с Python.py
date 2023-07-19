8c4ce92ce2a003779ced6c6f03f529c54a47af1a
Компьютер <vitamin48>
2023-07-19 11:06:18 +0300

    Initial commit

---
+++  selfedu/Уроки/1. Знакомство с Python.py
@@ -0,0 +1,8 @@
+"""
+https://stepik.org/lesson/566988
+https://www.youtube.com/watch?v=btuxcr7Sxw4&list=PLA0M1Bcd0w8yWHh2V70bTtbVxJICrnJHd
+"""
+
+"""При установке интерпретатора на windows обязательно ставим галочку: 'Add Python to Path' 
+Это позволит запускать интерпретатор python без необходимости указывать полного пути к нему. 
+Желательно устанавливать интерпретатор в путь, не содержащий кириллицы"""
